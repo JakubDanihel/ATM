de167884920f2da3ea1984cbcbc9d436570d8be6
JakubDanihel <108288883+JakubDanihel@users.noreply.github.com>
2022-06-28 21:27:07 +0200

    Delete App.java

--- App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception{
-        System.out.println("Hello, World!");
-        ID_PW id_pw = new ID_PW();
-        new ATM_panel(id_pw.getLoginInfo());
-    }
-}
